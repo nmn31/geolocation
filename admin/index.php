af58174c3191a450b330d40eb0c7fd16a28f8ffd
Narender Kumar <Narender Kumar>
2017-12-21 23:36:28 +0530

    sdfsfs

--- admin/index.php
+++ admin/index.php
@@ -30,13 +30,15 @@ print_r($locations);
   <div id="map" style="width: 500px; height: 400px;"></div>
 
   <script type="text/javascript">
-    var locations = [
+    /*var locations = [
 	  ['Bondi Beach', -33.890542, 151.274856, 4],
       ['Coogee Beach', -33.923036, 151.259052, 5],
       ['Cronulla Beach', -34.028249, 151.157507, 3],
       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
       ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+    ];*/
+	
+ var locations = <?php print_t($locations); ?>
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 2,
