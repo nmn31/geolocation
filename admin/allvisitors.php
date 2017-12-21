6093673c0f0c2a203e431fc1542a1b016544a86a
Narender Kumar <Narender Kumar>
2017-12-22 00:31:04 +0530

    sdfsfs

--- admin/allvisitors.php
+++ admin/allvisitors.php
@@ -1,16 +1,17 @@
 <?php 
 
 include"../db.php";
+
 $sql = "SELECT * FROM geo";
 $result = $conn->query($sql);
 $locations = [];
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        array_push($locations,[ $row["lati"] , $row["longi"] ]);
+      $locations[] = $row;
     }
 } else {
     echo "0 results";
 }
 
-print_r($locations);
+return json_encode($row) ;;
