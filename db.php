958b4f9a616965c34b8c42c9a37af25a31f412c6
Narender Kumar <Narender Kumar>
2017-12-22 01:54:55 +0530

    sdfsfs

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
+
+//https://linux7005.site4now.net:2083/cpsess6568782158/3rdparty/phpMyAdmin/sql.php?db=lx3krksj_geo&table=geo&pos=0
 $servername = "mysql7002.site4now.net";
 $username = "lx3krksj_narende";
 $password = "dxdev@296";
