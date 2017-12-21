daaf12ed504e65fd1f30216472ecf568df7a25f8
Narender Kumar <Narender Kumar>
2017-12-22 00:16:35 +0530

    sdfsfs

--- admin/index.php
+++ admin/index.php
@@ -32,46 +32,47 @@ print_r($locations);
   <div id="map" style="width: 500px; height: 400px;"></div>
 
   <script type="text/javascript">
-    var locations = [
+    /*var locations = [
 	  ['Bondi Beach', -33.890542, 151.274856, 4],
       ['Coogee Beach', -33.923036, 151.259052, 5],
       ['Cronulla Beach', -34.028249, 151.157507, 3],
       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
       ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
-var locations1;
+    ];*/
+var locations;
 $.ajax({
       type: 'POST',
       url: "allvisitors.php",
-      success: function(resultData) { alert("Save Complete") ; locations1 = resultData  ; console.log('');
-  console.log(locations1);}
+      success: function(resultData) { alert("Save Complete") ; locations = resultData  ; console.log('');
+      console.log(locations);\
+	  
+	     var map = new google.maps.Map(document.getElementById('map'), {
+		  zoom: 2,
+		  center: new google.maps.LatLng(-33.92, 151.25),
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+
+		var infowindow = new google.maps.InfoWindow();
+
+		var marker, i;
+
+		for (i = 0; i < locations.length; i++) {  
+		  marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		  });
+
+		  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			return function() {
+			  infowindow.setContent(locations[i][0]);
+			  infowindow.open(map, marker);
+			}
+		  })(marker, i));
+		}
+	  }
 });
-  console.log('dd');
-  console.log(locations1);
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 2,
-      center: new google.maps.LatLng(-33.92, 151.25),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
+  
+ 
   </script>
 </body>
 </html>
