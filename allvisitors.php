9c61597e9d675f631e15c4749dc3c1adafafefa7
Narender Kumar <Narender Kumar>
2017-12-22 00:07:38 +0530

    sdfsfs

---
+++ allvisitors.php
@@ -0,0 +1,16 @@
+<?php 
+
+include"../db.php";
+$sql = "SELECT * FROM geo";
+$result = $conn->query($sql);
+$locations = [];
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      $locations[] = $row
+    }
+} else {
+    echo "0 results";
+}
+
+print_r($row);
