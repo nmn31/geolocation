323b01653bdb5aae759d9fe9a4afbe7195363503
Narender Kumar <Narender Kumar>
2017-12-21 23:33:45 +0530

    sdfsfs

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <?php 
 include"../db.php";
-
 $sql = "SELECT * FROM geo";
 $result = $conn->query($sql);
+$locations = [];
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        array_push($locations,[ $row["lati"] , $row["longi"] ]);
+    }
+} else {
+    echo "0 results";
+}
 echo "<pre>";
-print_r($result);
+print_r($locations);
 ?>
 <html> 
 <head> 
